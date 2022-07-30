52c20889f7a11492a88ca9390ce762c385199a43
Hilton M. Cardoso <hilton.m.cardoso@gmail.com>
2022-07-30 13:25:30 -0300

    commit inicial

---
+++ src/components/Botao/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './styles.scss';
+
+class Botao extends React.Component {
+  render() {
+    return (
+      <button className="botao">
+        Botão
+      </button>
+    )
+  }
+}
+
+export default Botao;
